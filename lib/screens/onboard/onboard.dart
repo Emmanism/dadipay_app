3f48d452062a957f065e5da42674b0da7c78f923
alrhazes <anuuman2@gmail.com>
2023-09-29 14:26:16 +0100

    internet_access_commit

--- lib/screens/onboard/onboard.dart
+++ lib/screens/onboard/onboard.dart
@@ -1,11 +1,16 @@
 // ignore_for_file: use_full_hex_values_for_flutter_colors
 
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dadipay_app/routes/app_routes.dart';
 import 'package:dadipay_app/screens/onboard/controller/onboard_controller.dart';
+import 'package:dadipay_app/utils/error_handling.dart';
 import 'package:dadipay_app/utils/global_variables.dart';
 import 'package:dadipay_app/widgets/button_widget.dart';
 import 'package:dadipay_app/widgets/onboard_content.dart';
 import 'package:flutter/material.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Onboard extends StatefulWidget {
@@ -20,15 +25,65 @@ final AppRoutes appRoutes = AppRoutes();
 int currentPageIndex = 0;
 final pageController = PageController(initialPage: currentPageIndex);
 
-@override
-void dispose() {
-  pageController.dispose();
-}
-
-@override
-void initState() {}
-
 class _OnboardState extends State<Onboard> {
+  late StreamSubscription subscription;
+  var isConnected = false;
+  bool isAlert = false;
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    subscription.cancel();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    getConnectivity();
+    super.initState();
+  }
+
+  showConnectionDialog(BuildContext context, String title, String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () async {
+                Navigator.pop(context, 'Cancel');
+                setState(() => isAlert = false);
+                isConnected = await InternetConnectionChecker().hasConnection;
+                if (!isConnected) {
+                  showConnectionDialog(context, title, message);
+                  setState(() {
+                    isAlert = true;
+                  });
+                }
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  getConnectivity() {
+    subscription = Connectivity()
+        .onConnectivityChanged
+        .listen((ConnectivityResult result) async {
+      isConnected = await InternetConnectionChecker().hasConnection;
+      if (!isConnected && isAlert == false) {
+        showConnectionDialog(context, 'No Internet Connection',
+            'Please Check Your Internet Connection');
+        setState(() => isAlert = true);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
